94168de1189a3d4ce3343cd0946d3181e4a32772
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-09-25 19:24:49 +0530

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Counter extends Component{
+  constructor(){
+    super()
+    this.state={
+      temp:0
+    }
+    
+  }
+   
+  
+  plus=()=>{
+    this.setState(prevState=>{return {temp:prevState.temp+1 }})
+    }
+  minus=()=>{
+    this.setState(prevState=>{return {temp:prevState.temp-1 }})
+    }
+  reset=()=>{
+    this.setState({temp:0 })
+    }
+   
+    render(){
+    const {temp}=this.state
+    return(
+       <div className="container">
+          <h1>{temp}</h1>
+          <button onClick={this.plus} className="btn btn-primary mr-1">Increase</button>
+       </div>
+     )
+   }
+ }
+
+export default Counter;
